0099f6582a28295b1cb63f82eabf3dcd6a8b2129
arianna choza <unachoza@gmail.com>
2024-05-23 15:44:21 -0400

    added keyboard event listeners, and changed some functions from const to func for hoisting in useEffect to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState, useEffect, MouseEvent } from "react";
+import { useState, useEffect } from "react";
 import Table from "./components/Table/Table";
 import Button from "./components/Button/Button";
 import Robot from "./components/Robot/Robot";
 import Modal from "./components/Modal/Modal";
 import useScreenSize from "./utils/useScreenSize";
-import { INITIAL_ROBOT_STATE, INSTRUCTIONS, ROBOT_IMAGE_DIRECTIONS, TABLE_SIZE } from "./utils/constants";
+import { INITIAL_ROBOT_STATE, INSTRUCTIONS, ROBOT_IMAGE_DIRECTIONS } from "./utils/constants";
 import { Direction, RobotState } from "./utils/types";
 import "./App.css";
 
 const App = () => {
 	const [robotState, setRobotState] = useState<RobotState>(INITIAL_ROBOT_STATE);
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const toggling = () => setIsOpen(!isOpen);
 	const [modalText, setModalText] = useState<string>("");
 	const { screenSize, handleResize } = useScreenSize(setRobotState);
 	const [squarePxSize, setSquarePxSize] = useState<number>(120);
@@ -34,23 +33,39 @@ const App = () => {
 		}
 	}, [screenSize]);
 
-	const getRobotDirectionImage = () => {
-		console.log("called");
-		return ROBOT_IMAGE_DIRECTIONS[robotState.direction];
+	useEffect(() => {
+		const handleKeyPress = (event: KeyboardEvent) => {
+			switch (event.key) {
+				case "ArrowLeft":
+					robotState.location && handleLeft();
+					break;
+				case "ArrowRight":
+					robotState.location && handleRight();
+					break;
+				case "ArrowUp":
+					robotState.location && handleMove();
+					break;
+				default:
+					break;
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyPress);
+		return () => window.removeEventListener("keydown", handleKeyPress);
+	}, [handleMove, handleLeft, handleRight]);
+
+	const getNewDirection = (currentDirection: Direction, turn: "left" | "right"): Direction => {
+		let index = directions.indexOf(currentDirection);
+		index = turn === "left" ? (index + 3) % 4 : (index + 1) % 4;
+		return directions[index];
 	};
 
-	const isValidMove = (x: number, y: number): boolean => {
-		return x >= 0 && x < tableSize && y >= 0 && y < tableSize;
-	};
+	const isValidMove = (x: number, y: number): boolean => x >= 0 && x < tableSize && y >= 0 && y < tableSize;
 
-	const handleMove = () => {
+	function handleMove() {
 		if (robotState?.location) {
-			let {
-				location: { x, y },
-				direction,
-				top,
-				left,
-			} = robotState;
+			let { x, y } = robotState.location;
+			let { direction, top, left } = robotState;
 			switch (direction) {
 				case "north":
 					y += 1;
@@ -70,46 +85,33 @@ const App = () => {
 					break;
 			}
 			if (isValidMove(x, y)) {
-				setRobotState({
-					...robotState,
-					location: { x, y },
-					left,
-					top,
-				});
+				setRobotState({ ...robotState, location: { x, y }, left, top });
 			}
 		}
-	};
+	}
 
-	const handleChangeDirections = (e: MouseEvent<HTMLElement>) => {
-		if (robotState.location) {
-			let directionIndex: number = directions.indexOf(robotState.direction);
-			let directionChange = e.currentTarget.innerHTML.toLowerCase();
-			if (directionChange === "left") {
-				directionIndex = (directionIndex + 3) % 4;
-			} else if (directionChange === "right") {
-				directionIndex = (directionIndex + 1) % 4;
-			}
-			setRobotState((prevRobotState) => ({
-				...prevRobotState,
-				direction: directions[directionIndex] as Direction,
-			}));
-		}
-	};
+	function handleLeft() {
+		setRobotState({ ...robotState, direction: getNewDirection(robotState.direction, "left") });
+	}
 
+	function handleRight() {
+		setRobotState({ ...robotState, direction: getNewDirection(robotState.direction, "right") });
+	}
 	const handleReport = () => {
 		if (robotState?.location) {
-			const {
-				location: { x, y },
-				direction,
-			} = robotState;
+			const { x, y } = robotState.location;
+			const { direction } = robotState;
 			toggling();
 			setModalText(`Robot is located at ${x},${y} and is facing ${direction.toUpperCase()}`);
 		}
 	};
+
 	const showInstructions = () => {
 		toggling();
 		setModalText(INSTRUCTIONS);
 	};
+
+	const toggling = () => setIsOpen(!isOpen);
 
 	return (
 		<>
@@ -117,12 +119,17 @@ const App = () => {
 			<div className="app-container">
 				<Table setRobotState={setRobotState} />
 				<div>
-					<Robot image={getRobotDirectionImage()} x={robotState.left} y={robotState.top} location={robotState.location || null} />
+					<Robot
+						image={ROBOT_IMAGE_DIRECTIONS[robotState.direction]}
+						x={robotState.left}
+						y={robotState.top}
+						location={robotState.location || null}
+					/>
 					<div className="buttons-container">
 						<Button onClick={showInstructions} text="Instructions" />
 						<Button onClick={handleMove} text="Move" />
-						<Button onClick={(e) => handleChangeDirections(e)} text="Left" />
-						<Button onClick={(e) => handleChangeDirections(e)} text="Right" />
+						<Button onClick={handleLeft} text="Left" />
+						<Button onClick={handleRight} text="Right" />
 						<Button onClick={handleReport} text="Report" />
 					</div>
 					{isOpen && <Modal toggling={toggling} content={modalText} />}
